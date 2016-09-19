01354a3a02499dd4ca4f4be3f7956348c165011a
kurasawanko <kurasawanko96@gmail.com>
2016-09-19 05:10:46 +0000

    ページに詳細を追加

--- hello.rb
+++ hello.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  "<H1>Hello World! AIIT</H1>"
+<<EOS
+  <html>
+    <head>
+      <title>Hello World! AIIT</title>
+    </head>
+    <body>
+      <H1>Hello World! AIIT</H1>
+      <p>これはSinatraでWebアプリを作る演習です。</p>
+    </body>
+  </html>
+EOS
 end
