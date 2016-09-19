7f9f2961d121474b1d8d5675e52b71be17852b2f
kurasawanko <kurasawanko96@gmail.com>
2016-09-19 06:00:33 +0000

    viewファイルに切り出し

--- hello.rb
+++ hello.rb
@@ -1,17 +1,7 @@
 require 'sinatra'
 
 get '/' do
-<<EOS
-  <html>
-    <head>
-      <title>Hello World! AIIT</title>
-    </head>
-    <body>
-      <H1>Hello World! AIIT</H1>
-      <p>これはSinatraでWebアプリを作る演習です。</p>
-    </body>
-  </html>
-EOS
+  erb :index
 end
 
 get '/hello/:name' do |name|
